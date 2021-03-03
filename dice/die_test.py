1926a60a3e1791b9384eef32147683cb49bea8b3
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2021-03-03 20:45:19 +0100

    added test to check if greater than six

--- dice/die_test.py
+++ dice/die_test.py
@@ -6,7 +6,8 @@ from dice.die import Die
 class MyTestCase(unittest.TestCase):
     def test_not_less_than_one(self):
         self.assertGreaterEqual(Die().roll(), 1)
-
+    def test_not_greater_than_six(self):
+        self.assertLessEqual(Die().roll(), 6)
 
 if __name__ == '__main__':
     unittest.main()
