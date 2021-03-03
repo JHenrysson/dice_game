439a84530d31cee121970bc94bf965d0554539fa
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2021-03-03 20:36:56 +0100

    Added test for die can not roll less than one

--- dice/die_test.py
+++ dice/die_test.py
@@ -1,9 +1,11 @@
 import unittest
 
+from dice.die import Die
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        return
+    def test_not_less_than_one(self):
+        self.assertGreaterEqual(Die().roll(), 1)
 
 
 if __name__ == '__main__':
