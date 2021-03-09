f589e97a3f3ba54fc3a0a33beecf7eae9a819d1f
cougarhighland <71646812+cougarhighland@users.noreply.github.com>
2021-03-09 20:20:01 +0100

    rewrite code to fix in the game

--- dice/AI.py
+++ dice/AI.py
@@ -1,84 +1,59 @@
-import random
+"""AI module for computer game actions."""
+
+import die
+import dicehand
 
 
-class AI():
-    """computer player class. """
+class AI:
+    """AI class for game logic."""
 
-    def __init__(self):
-        def.AI_score = 0
-        def.list_AI_score = [0]
+    scores = []
+    total_rolls = 0
+    difficulty = 'easy'
+    turn = None
+    name = 'The computer'
 
-    def get_AI_score(self):
-        return self.AI_score
+    def __init__(self, difficulty):
+        """Initialise object and set difficulty."""
+        self.difficulty = difficulty
+        self.turn = dicehand.Dicehand()
 
-    def set_AI_score(self, score):
-        self.AI_score += score
+    def get_score(self):
+        """Return the total score for this game."""
+        return sum(self.scores)
 
-    def add_score_list(self, score):
-        self.list_AI_score.append(score)
+    def set_score(self, value):
+        """Add a score to the scores list."""
+        self.scores.append(value)
 
-    def get_last_score(self):
-        return self.list_AI_score[-1]
+    def reset_score(self):
+        """Reset object score to empty list."""
+        self.scores = []
 
-    def get_total_score(self):
-        return self.sum(list_AI_score)
+    def get_turns(self):
+        """Return the total amount of turns this game."""
+        return len(self.scores)
 
-    def easy_difficulty(self):
-    while get_total_score < 100:
-        rolled = random.randint(1, 6)
-        print('  Rolled %i' % rolled))
-           if rolled == 1:
-               print('Oop I roll 1, I loose %i this round, my score now is %i' %(get_AI_score, get_last_score))
-               set_AI_score(get_AI_score * -1)
-               break
+    def get_name(self):
+        """Return the name for AI."""
+        return self.name
+
+    def play(self):
+        """AI logic to take a turn."""
+        dice = die.Die()
+        hold_total = (7 if self.difficulty == 'easy' else 20)
+        while self.turn.get_total() < hold_total:
+            number = dice.roll()
+            print(f'Computer rolled a {number}')
+            if number == 1:
+                total = self.turn.get_total()
+                print(f"Computer rolled a {number} and lost {total} points")
+                self.turn.reset()
+                break
             else:
-                set_AI_score(rolled)
-        if 15 <= get_AI_score <= 20:
-            add_score_list(get_AI_score)
-            print(f'I\'ll hold and retain my {get_last_score} from this round')
-            break
-
-
-    def hard_difficulty(self):
-    while get_total_score < 100:
-        automatic_decision = should_roll(get_AI_score, get_total_score, player1_total)
-        if automatic_decision :
-            rolled = random.randint(1,6)
-           print('  Rolled %i' % rolled))
-           if rolled == 1:
-               print('Oop I roll 1, I loose %i this round, my score now is %i' %(get_AI_score, get_last_score))
-               set_AI_score(get_AI_score * -1)
-               break
-            else:
-                set_AI_score(rolled)
-        else:
-            add_score_list(get_AI_score)
-            print(f'I\'ll hold and retain my {get_last_score} from this round')
-            break
-
-
-    def should_roll(accumulated_score, AI_score, player_score)
-        AILead = AI_score - player_score
-        if player_score + AI_score >= 100:
-            return False
-        elif player_score >= 80 and AI_score < 80:
-            if accumulated_score <= 30:
-                return True
-            else:
-                return False
-        elif AI_score >= 80 and (0 < AILead and AILead <= 10):
-            if accumulated_score <= 15:
-                return True
-            else:
-                return False
-        else:
-            if accumulated_score <= 20:
-                return True
-            else:
-                return False
-        
-
-
-
-
-
+                self.turn.increment(number)
+                if self.get_score() + self.turn.get_total() >= 10:
+                    break
+        total = self.turn.get_total()
+        self.set_score(total)
+        self.turn.reset()
