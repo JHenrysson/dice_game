a83139a978d31aebe65f082b9f507ed3da00b204
Paul Miller <paul_john.miller0008@stud.hkr.se>
2021-03-10 22:16:08 +0100

    fixed some tests in ai_test

--- dice/ai_test.py
+++ dice/ai_test.py
@@ -1,11 +1,11 @@
 """unittest ai."""
 import unittest
 import ai
-import dicehand
-import die
+
 
 class TestaiClass(unittest.TestCase):
     """Test for ai class."""
+
     def test_init_ai_object(self):
         """Test object."""
         res = ai.AI("easy")
@@ -15,37 +15,42 @@ class TestaiClass(unittest.TestCase):
     def test_get_score(self):
         """Test get score function."""
         test = ai.AI("easy")
-        test.scores = [1,2,3]
+        test.scores = [1, 2, 3]
         res = test.get_score()
         exp = 6
         self.assertEqual(res, exp)
-        
+
     def test_set_score(self):
+        """Check score is updated correctly."""
         test = ai.AI("easy")
-        test.scores = [1,2,3]
+        test.scores = [1, 2, 3]
         test.set_score(4)
         res = 4 in test.scores
         self.assertTrue(res)
 
     def test_reset_score(self):
+        """Check score list is reset to empty list."""
         test = ai.AI("easy")
         test.scores = [1]
-        test.reset_score
-        res = not test.scores
-        self.assertTrue(res)
+        test.reset_score()
+        res = len(test.scores)
+        exp = 0
+        self.assertEqual(res, exp)
 
     def test_get_name(self):
         """Test get name function."""
         test = ai.AI("easy")
-        exp = test.get_name == 'PIG Rookie' or 'PIG Expert'
-        self.assertTrue(exp)
-        
+        res = test.get_name()
+        exp = 'PIG Rookie'
+        self.assertEqual(res, exp)
+
     def test_set_name(self):
         """Test set name function."""
         test = ai.AI("easy")
         test.set_name("paul")
         exp = test.name == "paul"
         self.assertTrue(exp)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
