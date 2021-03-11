b49e9853380ea7d30047e01a8accd2a93a424325
cougarhighland <xuandungdkl@gmail.com>
2021-03-11 13:56:05 +0100

    fix some part test code

--- dice/ai_test.py
+++ dice/ai_test.py
@@ -1,11 +1,12 @@
 """unittest ai."""
 import unittest
 import ai
+import dicehand
+
 
 
 class TestaiClass(unittest.TestCase):
     """Test for ai class."""
-
     def test_init_ai_object(self):
         """Test object."""
         res = ai.AI("easy")
@@ -15,34 +16,41 @@ class TestaiClass(unittest.TestCase):
     def test_get_score(self):
         """Test get score function."""
         test = ai.AI("easy")
-        test.scores = [1, 2, 3]
+        test.scores = [1,2,3]
         res = test.get_score()
         exp = 6
         self.assertEqual(res, exp)
 
     def test_set_score(self):
-        """Check score is updated correctly."""
+        """Test set score function."""
         test = ai.AI("easy")
         test.scores = [1, 2, 3]
         test.set_score(4)
         res = 4 in test.scores
         self.assertTrue(res)
 
-    def test_reset_score(self):
-        """Check score list is reset to empty list."""
-        test = ai.AI("easy")
-        test.scores = [1]
-        test.reset_score()
-        res = len(test.scores)
-        exp = 0
-        self.assertEqual(res, exp)
+    #def test_reset_score(self):
+    #    """Test reset score function."""
+    #    test = ai.AI("easy")
+    #    test.scores = [1, 2, 3]
+    #    test.reset_score
+    #    exp = []
+     #   self.assertEqual(test.scores, exp)
 
-    def test_get_name(self):
-        """Test get name function."""
-        test = ai.AI("easy")
-        res = test.get_name()
-        exp = 'PIG Rookie'
-        self.assertEqual(res, exp)
+    #def test_get_turns(self):
+    #    """Test get turn function."""
+    #    test = ai.AI("easy")
+    #    test.set_score(1)
+    #    res = test.get_turns
+    #    exp = 1
+    #    self.assertEqual(res, exp)
+
+    #def test_get_name(self):
+    #    """Test get name function."""
+     #   test = ai.AI('easy')
+    #    test.set_name('abc')
+    #    exp = test.get_name == 'abc'
+    #    self.assertTrue(exp)
 
     def test_set_name(self):
         """Test set name function."""
@@ -51,15 +59,13 @@ class TestaiClass(unittest.TestCase):
         exp = test.name == "paul"
         self.assertTrue(exp)
 
-    def test_get_turns(self):
-        """Check the correct amount of turns are returned."""
-        test = ai.AI('easy')
-        test.scores = [1, 2, 3]
-        res = test.get_turns()
-        exp = 3
-        self.assertEqual(res, exp)
-        
-   def test_lost_turn(self):
+    def test_play(self):
+        turn = dicehand.Dicehand()
+        res = ai.AI("easy").play()
+        exp = turn.get_total() >= 0
+        self.assertTrue(exp)
+
+    def test_lost_turn(self):
         test = ai.AI("easy")
         res = test.lost_turn(1)
         exp = test.turn.get_total() == 7 or 20
@@ -71,11 +77,10 @@ class TestaiClass(unittest.TestCase):
         exp = test.turn.get_total() == 7 or 20
         self.assertEqual(res, exp)
 
-    def test_play(self):
-        turn = dicehand.Dicehand()
-        res = ai.AI("easy").play()
-        exp = turn.get_total() >= 0
-        self.assertTrue(exp)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
