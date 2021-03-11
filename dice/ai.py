c01f138d21c49d08016a2c8778691bfef21e3999
Paul Miller <paul_john.miller0008@stud.hkr.se>
2021-03-11 19:32:05 +0100

    Fixed AI play method and tests

--- dice/ai.py
+++ dice/ai.py
@@ -7,14 +7,13 @@ import dicehand
 class AI:
     """AI class for game logic."""
 
-    scores = []
-    total_rolls = 0
-
     def __init__(self, difficulty):
         """Initialise object and set difficulty."""
         self.difficulty = difficulty
         self.turn = dicehand.Dicehand()
         self.name = ('PIG Rookie' if difficulty == 'easy' else 'PIG Expert')
+        self.dice = die.Die()
+        self.scores = []
 
     def get_score(self):
         """Return the total score for this game."""
@@ -42,30 +41,25 @@ class AI:
 
     def play(self):
         """AI logic to take a turn."""
-        dice = die.Die()
         hold_total = (7 if self.difficulty == 'easy' else 20)
 
         while self.turn.get_total() < hold_total:
-            number = dice.roll()
-            self.lost_turn(number)
-            
-            print(f'{self.name} rolled a {number}\n')
-            self.turn.increment(number)
-            self.max_score(self.get_score(),self.turn.get_total())
+            number = self.dice.roll()
 
+            if number == 1:
+                total = self.turn.get_total()
+                print(f"{self.name} rolled a {number} and lost {total} points\n")
+                self.turn.reset()
+                break
+            else:
+                print(f'{self.name} rolled a {number}\n')
+                self.turn.increment(number)
+                total = self.turn.get_total()
+                if self.get_score() + total >= 100:
+                    self.set_score(total)
+                    break  # Break the loop if winning score
 
         total = self.turn.get_total()
         self.set_score(total)
         print(f"{self.name} scored {total} points this turn\n")
         self.turn.reset()
-
-    def lost_turn(self, number):
-        if number == 1:
-            total = self.turn.get_total()
-            print(f"{self.name} rolled a {number} and lost {total} points\n")
-            self.turn.reset()
-            return self.turn.get_total() == 7 or 20  # Break the loop, turn has ended (rolled a 1)
-
-    def max_score(self, accumulated_score, total_score):
-        if accumulated_score + total_score >= 100:
-            return self.turn.get_total() == 7 or 20  # Break the loop if winning score
--- dice/ai_test.py
+++ dice/ai_test.py
@@ -1,12 +1,13 @@
 """unittest ai."""
 import unittest
+from unittest.mock import MagicMock
 import ai
 import dicehand
 
 
-
-class TestaiClass(unittest.TestCase):
+class TestAiClass(unittest.TestCase):
     """Test for ai class."""
+
     def test_init_ai_object(self):
         """Test object."""
         res = ai.AI("easy")
@@ -16,7 +17,7 @@ class TestaiClass(unittest.TestCase):
     def test_get_score(self):
         """Test get score function."""
         test = ai.AI("easy")
-        test.scores = [1,2,3]
+        test.scores = [1, 2, 3]
         res = test.get_score()
         exp = 6
         self.assertEqual(res, exp)
@@ -29,28 +30,28 @@ class TestaiClass(unittest.TestCase):
         res = 4 in test.scores
         self.assertTrue(res)
 
-    #def test_reset_score(self):
-    #    """Test reset score function."""
-    #    test = ai.AI("easy")
-    #    test.scores = [1, 2, 3]
-    #    test.reset_score
-    #    exp = []
-     #   self.assertEqual(test.scores, exp)
+    def test_reset_score(self):
+        """Test reset score function."""
+        test = ai.AI("easy")
+        test.scores = [1, 2, 3]
+        test.reset_score()
+        exp = []
+        self.assertEqual(test.scores, exp)
 
-    #def test_get_turns(self):
-    #    """Test get turn function."""
-    #    test = ai.AI("easy")
-    #    test.set_score(1)
-    #    res = test.get_turns
-    #    exp = 1
-    #    self.assertEqual(res, exp)
+    def test_get_turns(self):
+        """Test get turn function."""
+        test = ai.AI("easy")
+        test.set_score(1)
+        res = test.get_turns()
+        exp = 1
+        self.assertEqual(res, exp)
 
-    #def test_get_name(self):
-    #    """Test get name function."""
-     #   test = ai.AI('easy')
-    #    test.set_name('abc')
-    #    exp = test.get_name == 'abc'
-    #    self.assertTrue(exp)
+    def test_get_name(self):
+        """Test get name function."""
+        test = ai.AI('easy')
+        test.set_name('abc')
+        exp = test.get_name() == 'abc'
+        self.assertTrue(exp)
 
     def test_set_name(self):
         """Test set name function."""
@@ -59,27 +60,34 @@ class TestaiClass(unittest.TestCase):
         exp = test.name == "paul"
         self.assertTrue(exp)
 
-    def test_play(self):
-        turn = dicehand.Dicehand()
-        res = ai.AI("easy").play()
-        exp = turn.get_total() >= 0
-        self.assertTrue(exp)
-
-    def test_lost_turn(self):
-        test = ai.AI("easy")
-        res = test.lost_turn(1)
-        exp = test.turn.get_total() == 7 or 20
+    def test_play_rolls_one(self):
+        """Test object updated correctly when rolling 1."""
+        obj = ai.AI('easy')
+        obj.dice.roll = MagicMock(return_value=1)
+        obj.play()
+        res = obj.get_score()
+        exp = 0
         self.assertEqual(res, exp)
 
-    def test_max_score(self):
-        test = ai.AI("easy")
-        res = test.max_score(50, 60)
-        exp = test.turn.get_total() == 7 or 20
+    def test_play_rolls_not_one(self):
+        """Test object updated correctly when not rolling 1."""
+        obj = ai.AI('hard')
+        obj.dice.roll = MagicMock(return_value=6)
+        obj.play()
+        print(obj.scores)
+        res = obj.get_score()
+        exp = 24
         self.assertEqual(res, exp)
 
-
-
-
+    def test_play_breaks_loop(self):
+        """Check play method breaks loop when score >= 100."""
+        obj = ai.AI('easy')
+        obj.set_score(99)
+        obj.dice.roll = MagicMock(return_value=6)
+        obj.play()
+        res = obj.get_score()
+        exp = 105
+        self.assertEqual(res, exp)
 
 
 if __name__ == '__main__':
